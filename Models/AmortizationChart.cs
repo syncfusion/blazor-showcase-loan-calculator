5ff5fbe41102fe0179fedd8b46fe43e032fa5c40
Muthukumar Kannan <muthukumar.kannan@syncfusion.com>
2023-06-16 22:27:17 +0530

    Adding Application

---
+++ Models/AmortizationChart.cs
@@ -0,0 +1,10 @@
+﻿namespace LoanCalculator.Models
+{
+    public class AmortizationChart
+    {
+        public double XValue { get; set; }
+        public double YValue { get; set; }
+
+        public static List<AmortizationChart> ChartData = new List<AmortizationChart>();
+    }
+}

09f332c68ff81a68f70ccd88e78a3c56329e40be
athiswaransf3980 <athiswaran.sf3980@gmail.com>
2024-09-30 17:14:42 +0530

    911856: Update sample for vol3 2024

--- webapp/wasm/LoanCalculator/LoanCalculator.Client/Program.cs
+++
@@ -1,9 +0,0 @@
-using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Syncfusion.Blazor;
-using Blazored.LocalStorage;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddSyncfusionBlazor();
-builder.Services.AddBlazoredLocalStorage();
-await builder.Build().RunAsync();
